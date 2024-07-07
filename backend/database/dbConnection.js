5fefd7da0ae452ce8e157873212d3fa4d922dd79
AmmarMotaz <127546796+ammar-motaz2001@users.noreply.github.com>
2024-07-07 15:34:30 +0200

    Fix Uploading
    This Right Project and old upload was wrong project

---
+++ backend/database/dbConnection.js
@@ -0,0 +1,15 @@
+import { Sequelize } from "sequelize" //EC6 
+
+//connection to database by ORM (Sequalize)
+const dbConnection=new Sequelize('task mangement system','root','',{
+    host:"localhost",
+    dialect:"mysql" //
+})
+
+dbConnection.authenticate().then(()=>{ // to ensure that the connect of database is done or not 
+    console.log("Database is Connected")
+}).catch((err)=>{
+    console.log("Database disConnected")
+})
+
+export default dbConnection
--- backend/src/component/Signin.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Signin() {
-  return (
-    <>
-     <h1>Sign In</h1> 
-    </>
-  )
-}
